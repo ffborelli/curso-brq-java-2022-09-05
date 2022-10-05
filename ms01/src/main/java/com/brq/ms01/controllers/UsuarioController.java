2d97692db4b62a689f1910cdec266d94f0f48f51
ffborelli <ffborelli@gmail.com>
2022-10-04 22:26:14 -0300

    Lombok

--- ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
+++ ms01/src/main/java/com/brq/ms01/controllers/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.brq.ms01.controllers;
 
+import com.brq.ms01.models.UsuarioModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 
 // comentário
 
@@ -12,11 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UsuarioController {
 
+    // ESTE ARRAYLIST É DIDÁTICO, POIS ESTÁ SIMULANDO UM BANCO DE DADOS
+    private ArrayList<UsuarioModel> usuarios = new ArrayList<>();
+
     /*
     * o @GetMapping permite associoar o verbo GET com a rota /usuarios
     * */
     @GetMapping("usuarios")
-    public String getAllUsuarios(){
-        return "GET Usuários";
+    public ArrayList<UsuarioModel> getAllUsuarios(){
+
+        UsuarioModel u = new UsuarioModel();
+        u.setId(1);
+        u.setNome("Manoel");
+        u.setEmail("manoel@gmail.com");
+
+        usuarios.add(u);
+
+        return usuarios;
     }
+
+    @PostMapping("usuarios")
+    public String create(){
+        return "POST Usuários";
+    }
+
 }
