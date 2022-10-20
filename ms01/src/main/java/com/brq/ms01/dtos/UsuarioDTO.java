607dea70714ce566da03670443f999291d68bad9
ffborelli <ffborelli@gmail.com>
2022-10-20 19:51:24 -0300

    feat: DTO

--- ms01/src/main/java/com/brq/ms01/dtos/UsuarioDTO.java
+++ ms01/src/main/java/com/brq/ms01/dtos/UsuarioDTO.java
@@ -1,8 +1,10 @@
 package com.brq.ms01.dtos;
 
+import com.brq.ms01.models.UsuarioModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +15,13 @@ public class UsuarioDTO {
     private String nome;
     private String email;
     private String telefone;
+
+    public UsuarioModel toModel(){
+
+        ModelMapper mapper = new ModelMapper();
+
+        UsuarioModel model = mapper.map(this , UsuarioModel.class );
+
+        return model;
+    }
 }
