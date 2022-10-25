ef09647ca546df9952a47f6c7f22225e298d54d7
ffborelli <ffborelli@gmail.com>
2022-10-25 19:31:39 -0300

    feat: métodos da exceção

--- ms01/src/main/java/com/brq/ms01/exceptions/ResourceExceptionHandler.java
+++ ms01/src/main/java/com/brq/ms01/exceptions/ResourceExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.brq.ms01.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
 
 /*
 * @ControllerAdvice permite manipular exceções de forma global.
@@ -16,17 +19,22 @@ public class ResourceExceptionHandler {
     /* gostaria que o método abaixo trate exceções
         do tipo de validação de dados*/
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void methodValidationHandler(){
+    public void methodValidationHandler(
+            MethodArgumentNotValidException exception,
+            HttpServletRequest request
+            ){
 
-        StandardError standardError = new StandardError();
-        standardError.setStatus(400);
-        standardError.setPath("");
+//        StandardError standardError = new StandardError();
+//        standardError.setStatus(400);
+//        standardError.setPath("");
 
-// @Builder
-//        StandardError standardError = StandardError
-//                            .builder()
-//                            .status(400)
-//                            .path("")
-//                            .build();
+ // @Builder
+        StandardError standardError = StandardError
+                            .builder()
+                            .timestamp(System.currentTimeMillis())
+                            .status(HttpStatus.BAD_REQUEST.value())
+                            //.error()
+                            .path("")
+                            .build();
     }
 }
