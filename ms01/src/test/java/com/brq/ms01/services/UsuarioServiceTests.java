1175188b0cb09826d28fdfc3bbe0a5245e773d67
ffborelli <ffborelli@gmail.com>
2022-10-31 20:02:49 -0300

    Update UsuarioServiceTests.java

--- ms01/src/test/java/com/brq/ms01/services/UsuarioServiceTests.java
+++ ms01/src/test/java/com/brq/ms01/services/UsuarioServiceTests.java
@@ -1,11 +1,18 @@
 package com.brq.ms01.services;
 
+import com.brq.ms01.models.UsuarioModel;
 import com.brq.ms01.repositories.UsuarioRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class UsuarioServiceTests {
@@ -19,6 +26,20 @@ public class UsuarioServiceTests {
 
     @Test
     void getAllUsuariosTest(){
+        // o primeiro passo é simular (mockar) os objetos que preciso
+        List<UsuarioModel> listMock = new ArrayList<>();
 
+        UsuarioModel usuarioModel = new UsuarioModel();
+        usuarioModel.setId(1);
+        usuarioModel.setNome("Teste");
+        usuarioModel.setTelefone("Meu telefone");
+
+        listMock.add(usuarioModel);
+
+        // quando o findAll da camada repository for acionado, retorno a lista acima
+        when ( usuarioRepository.findAll() )
+                .thenReturn( listMock );
+
+        // 
     }
 }
