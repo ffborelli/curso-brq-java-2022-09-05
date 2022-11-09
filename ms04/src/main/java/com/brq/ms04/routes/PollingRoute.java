eec8e66ef3e979f731c0a80ca178c3894144f1bb
ffborelli <ffborelli@gmail.com>
2022-11-08 21:30:33 -0300

    feat: @Value

--- ms04/src/main/java/com/brq/ms04/routes/PollingRoute.java
+++ ms04/src/main/java/com/brq/ms04/routes/PollingRoute.java
@@ -1,13 +1,20 @@
 package com.brq.ms04.routes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
+@Slf4j
 @Component
 public class PollingRoute extends RouteBuilder {
 
+    @Value("${exchange.url}")
+    private String url;
+
     @Override
     public void configure() throws Exception {
+
+        log.info("O valor da URL é {}", url);
 
         // após o caracter ?, podemos colocar os parâmetros do conector do camel
         // ex: period=5000 -> executar a cada 5 segundos
