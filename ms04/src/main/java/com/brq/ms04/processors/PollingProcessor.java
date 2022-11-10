b53e14c4ae9a42347296a3264ca9e1910cb8911e
ffborelli <ffborelli@gmail.com>
2022-11-09 21:31:07 -0300

    feat: models

--- ms04/src/main/java/com/brq/ms04/processors/PollingProcessor.java
+++ ms04/src/main/java/com/brq/ms04/processors/PollingProcessor.java
@@ -1,6 +1,8 @@
 package com.brq.ms04.processors;
 
 
+import com.brq.ms04.models.CotacaoUSD;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
@@ -9,5 +11,12 @@ public class PollingProcessor implements Processor {
     public void process(Exchange exchange) throws Exception {
         final var messageIn = exchange.getIn().getBody(String.class);
         System.out.println("GET IN " + messageIn);
+
+        final var objectMapper = new ObjectMapper();
+
+        final var cotacao = objectMapper
+                        .readValue(messageIn, CotacaoUSD.class);
+
+        System.out.println("GET OUT " + cotacao);
     }
 }
