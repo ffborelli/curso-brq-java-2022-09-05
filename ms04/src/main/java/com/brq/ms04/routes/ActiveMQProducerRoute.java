90d65140c3c14e1058be900a43fa52fb71c0ea55
ffborelli <ffborelli@gmail.com>
2022-11-11 20:09:23 -0300

    feat: ActiveMQProducerRoute

--- ms04/src/main/java/com/brq/ms04/routes/ActiveMQProducerRoute.java
+++ ms04/src/main/java/com/brq/ms04/routes/ActiveMQProducerRoute.java
@@ -1,7 +1,11 @@
 package com.brq.ms04.routes;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
+
+import java.util.Random;
 
 @Component
 public class ActiveMQProducerRoute extends RouteBuilder {
@@ -9,8 +13,16 @@ public class ActiveMQProducerRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("timer:active-mq-producer?repeatCount=10&period=3000")
-                .transform().constant("Apache Camel")
-                .to("activemq:meu-primeiro-topico");
+        from("timer:active-mq-producer?period=1000")
+                //.transform().constant("Apache Camel")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        Random random = new Random();
+                        exchange.getIn().setBody("Apache Camel " + random.nextInt());
+                    }
+                })
+                .log("${body}")
+                .to("activemq:meu-primeiro-topico-2");
     }
 }
