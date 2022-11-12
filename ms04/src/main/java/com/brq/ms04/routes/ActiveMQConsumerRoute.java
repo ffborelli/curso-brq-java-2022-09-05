dc18f8ce639c931b60298e9ed01df9d7ed8cea47
ffborelli <ffborelli@gmail.com>
2022-11-11 21:34:13 -0300

    feat: salvando dados vindo da fila salvando no banco de dados

--- ms04/src/main/java/com/brq/ms04/routes/ActiveMQConsumerRoute.java
+++ ms04/src/main/java/com/brq/ms04/routes/ActiveMQConsumerRoute.java
@@ -1,5 +1,6 @@
 package com.brq.ms04.routes;
 
+import com.brq.ms04.beans.MessageInBean;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ public class ActiveMQConsumerRoute extends RouteBuilder {
     public void configure() throws Exception {
 
         from("activemq:meu-primeiro-topico-2")
+                .bean(MessageInBean.class, "save")
                 .log("LOG CONSUMER >>> ${body}");
     }
 }
