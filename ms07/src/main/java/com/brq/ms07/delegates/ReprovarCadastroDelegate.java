26d55be9b158831bd5452ea4cd7a7fb296541a23
ffborelli <ffborelli@gmail.com>
2022-12-08 20:51:46 -0300

    feat: salvando idade no banco de dados

--- ms07/src/main/java/com/brq/ms07/delegates/ReprovarCadastroDelegate.java
+++ ms07/src/main/java/com/brq/ms07/delegates/ReprovarCadastroDelegate.java
@@ -4,6 +4,10 @@ import javax.inject.Named;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.brq.ms07.models.CadastroModel;
+import com.brq.ms07.services.ICadastroService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,6 +15,9 @@ import lombok.extern.slf4j.Slf4j;
 @Named("reprovarCadastro")
 public class ReprovarCadastroDelegate
 	implements JavaDelegate{
+	
+	@Autowired
+	private ICadastroService service;
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
@@ -18,5 +25,12 @@ public class ReprovarCadastroDelegate
 		final var idade = (Integer) execution.getVariable("idade");
 		
 		log.info("Reprovar Cadastro : idade {}", idade);
+		
+		CadastroModel model = new CadastroModel();
+		model.setIdade(idade);
+		
+		service.create(model);
+
+
 	}
 }
