9235a3b83faf9795cfefd000827c437d4c7706ff
ffborelli <ffborelli@gmail.com>
2022-12-13 20:08:31 -0300

    feat: teste do getOne, create e update

--- ms06/src/test/java/com/brq/ms06/services/UsuarioServiceTest.java
+++ ms06/src/test/java/com/brq/ms06/services/UsuarioServiceTest.java
@@ -1,6 +1,7 @@
 package com.brq.ms06.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMostOnce;
 import static org.mockito.Mockito.doNothing;
@@ -10,6 +11,7 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 import org.junit.jupiter.api.Test;
@@ -24,7 +26,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.brq.ms06.exceptions.NaoAcheiException;
 import com.brq.ms06.models.UsuarioModel;
 import com.brq.ms06.repositories.UsuarioRepository;
 import com.brq.ms06.utils.Utils;
@@ -71,6 +75,122 @@ public class UsuarioServiceTest {
 			.isEqualTo(listEntity.get(0).getEmail());
 		
 	} 
+	
+	@Test
+	void getOneWhenFindUser() {
+		
+		// dado que
+		String id = "1";
+		String nome = "nome";
+		String email = "email";
+		
+		final var usuarioModel = getUsuarioModelMock(id, nome, email);
+		final var optional = Optional.of(usuarioModel);
+		
+		// quando
+		when(repository.findById(id)).thenReturn(optional);
+		
+		// então
+		final var response = service.getOne(id);
+		
+		// validar teste
+		assertThat(response.getEmail()).isEqualTo(email);
+		assertThat(response.getNome()).isEqualTo(nome);
+		assertThat(response.getId()).isEqualTo(id);
+	}
+	
+	@Test
+	void getOneNotWhenFindUser() {
+		
+		// dado que
+		String id = "1";
+		Optional<UsuarioModel> optional = Optional.empty();
+		
+		// quando
+		when(repository.findById(id)).thenReturn(optional);
+		
+		// então
+		assertThrows( NaoAcheiException.class, () -> service.getOne(id));
+		
+		// verificar testes
+	}
+	
+	@Test
+	void createTest() {
+		
+		// dado que
+		String id = "1";
+		String nome = "nome";
+		String email = "email";
+		
+		final var usuarioModel = getUsuarioModelMock(id, nome, email);
+		
+		// quando
+		when(repository.save(usuarioModel)).thenReturn(usuarioModel);
+		
+		// então
+		final var response = service.create(usuarioModel);
+		
+		//verificar testes
+		assertThat(response.getEmail()).isEqualTo(email);
+		assertThat(response.getNome()).isEqualTo(nome);
+		assertThat(response.getId()).isEqualTo(id);
+	}
+	
+	@Test
+	void updateWhenFindUser() {
+		
+		// dado que
+		String id = "1";
+		String nome = "nome";
+		String email = "email";
+		String nomeAlterado = "nomeAlterado";
+		String emailAlterado = "emailAlterado";
+		
+		final var usuarioModel = getUsuarioModelMock(id, nome, email);
+		final var optional = Optional.of(usuarioModel);
+		
+		final var usuarioModelAlterado = getUsuarioModelMock(id, nomeAlterado, emailAlterado);
+		final var optionalAlterado = Optional.of(usuarioModelAlterado);
+		
+		// quando
+		when(repository.findById(id)).thenReturn(optional);
+		when(repository.save(usuarioModelAlterado)).thenReturn(usuarioModelAlterado);
+		
+		// então
+		final var response = service.update(id, usuarioModelAlterado.toDTO());
+		
+		// validar o teste
+		assertThat(response.getNome())
+			.isEqualTo(usuarioModelAlterado.getNome());
+		
+		assertThat(response.getEmail())
+		.isEqualTo(usuarioModelAlterado.getEmail());
+		
+		assertThat(response.getId())
+		.isEqualTo(usuarioModelAlterado.getId());
+	}
+	
+	@Test
+	void updateWhenNotFindUser() {
+		
+		// dado que
+		String id = "1";
+		String nomeAlterado = "nomeAlterado";
+		String emailAlterado = "emailAlterado";
+		
+		final var usuarioModelAlterado = getUsuarioModelMock(id, nomeAlterado, emailAlterado);
+		Optional<UsuarioModel> optional = Optional.empty();
+
+		// quando
+		when(repository.findById(id)).thenReturn(optional);
+		
+		// então
+		assertThrows(NaoAcheiException.class, 
+					() -> service.update(id, 
+							usuarioModelAlterado.toDTO())) ;
+	}
+
 	
 	@Test
 	void findByEmailTest() {
